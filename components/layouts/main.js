73280cba47a8a7661d72059fad62f1800237a023
Kenz <oakleysdmr@gmail.com>
2022-02-23 14:44:40 -0800

    changed title

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -12,7 +12,7 @@ const Main = ({ children, router }) => {
           name="viewport"
           content="width=device-width, initial-scale=1"
         ></meta>
-        <title>Kenz Dempsey - Vancouver Web Developer | </title>
+        <title>Kenz Dempsey - Vancouver Web Developer</title>
       </Head>
 
       <Navbar path={router.asPath} />
